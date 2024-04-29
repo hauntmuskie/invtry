4b61d8ba052fe4904e92cd7af2c7795daba95562
Rionggo Rahardi <rioclasher4@gmail.com>
2024-04-29 21:48:57 +0700

    Add java.sql module requirement and Database class

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.lestarieragemilang {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
     opens com.lestarieragemilang to javafx.fxml;
     exports com.lestarieragemilang;
